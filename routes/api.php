adc58e7f64cf9b7700c643646aee224fbe76b288
mortezaa <mortezajafari76@gmail.com>
2025-09-25 14:55:01 +0330

    fix:issues

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Coupons\Http\Controllers\CouponController;
-
-Route::get('coupons', [CouponController::class, 'index'])->name('coupons.index');
-Route::get('coupons/{coupon}', [CouponController::class, 'show'])->name('coupons.show');
+Route::prefix('api')->group(function () {
+    Route::get('coupons', [CouponController::class, 'index'])->name('coupons.index');
+    Route::get('coupons/{coupon}', [CouponController::class, 'show'])->name('coupons.show');
+});
