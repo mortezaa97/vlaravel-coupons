e9c6cf51c62460841187d7359678b426ad312f84
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:37 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Coupons\Http\Controllers\CouponController;
-Route::prefix('api')->group(function () {
+Route::prefix('api')->middleware('api')->group(function () {
     Route::get('coupons', [CouponController::class, 'index'])->name('coupons.index');
     Route::get('coupons/{coupon}', [CouponController::class, 'show'])->name('coupons.show');
 });
